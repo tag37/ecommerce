bd503d7367390284c8a91b754d5efa2a38e9a50d
Tushar Ghulaxe <tushar.ghulaxe37@gmail.com>
2019-06-12 00:42:04 +0530

    Added category page for adding category

---
+++ admin_BigKisan/admin_BigKisan/view/category/category.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace admin_BigKisan.view.category
+{
+    public partial class Category : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            var listItem = new Dictionary<string, string>
+            {
+                {"1", "Category 1"}, {"2", "Category 2"}, {"3", "Category 3"}, {"4", "Category 4"}
+            };
+            ddlParentCategory.DataSource = listItem;
+            ddlParentCategory.DataBind();
+        }
+
+        protected void btnSubmit_OnClick(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
--- admin_BigKisan/admin_BigKisan/view/manage-category.aspx.cs
+++ admin_BigKisan/admin_BigKisan/view/manage-category.aspx.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace admin_BigKisan.view
 {
-	public partial class manage_category : System.Web.UI.Page
+    public partial class manage_category : System.Web.UI.Page
 	{
 		protected void Page_Load(object sender, EventArgs e)
 		{
